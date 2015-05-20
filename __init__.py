1813cc33198eac6b8710ac9e65c5f739fa1d7b76
Casper Steinmann <casper.steinmann@gmail.com>
2015-05-20 16:03:38 +0200

    Improves __ini__ a bit.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,28 @@
+"""
+https://gitlab.com/cstein/pepylib
+
+The Polarizable Embedding Python Library gives pythonic access
+to PE potential files.
+
+Modules:
+
+- potential: Class to manipulate PE potentials
+
+- solvers: Solvers for induced charges and moments
+
+- version: holds the current API version
+
+"""
+
+__all__ = ['potential', 'solvers']
 __author__ = "Casper Steinmann"
 __copyright__ = "Copyright 2015, Casper Steinmann"
 __license__ = "BSD 2-clause"
-__version__ = "0.0.1"
 __maintainer__ = "Casper Steinmann"
 __email__ = "casper.steinmann@gmail.com"
 __status__ = "Development"
+
+from . version import __version__
+import potential
+import solvers
+
